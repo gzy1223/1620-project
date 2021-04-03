ff17095b98e9860195a55c7bbfe1e38e11ad4ea6
gzy1223 <56465865+gzy1223@users.noreply.github.com>
2021-04-03 23:39:57 +0800

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -2,7 +2,10 @@
 #include "stm32f413h_discovery_ts.h"
 #include "stm32f413h_discovery_lcd.h"
 
-TS_StateTypeDef TS_State = { 0 };
+#define SCREEN_X_LEN 240
+#define SCREEN_Y_LEN 240
+
+TS_StateTypeDef TS_State = {0};
 struct position
 {
     int x;
@@ -12,9 +15,9 @@ struct position
 class iniTouchscreen
 {
 private:
-    
 public:
     void initial();
+    int touch();
 };
 /*Touch screen CPP file*/
 
@@ -25,40 +28,77 @@ void iniTouchscreen::initial()
     BSP_LCD_Init();
 
     /* Touchscreen initialization */
-    if (BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize()) == TS_ERROR) {
+    if (BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize()) == TS_ERROR)
+    {
         printf("BSP_TS_Init error\n");
     }
-        /* Clear the LCD */
+    /* Clear the LCD */
     BSP_LCD_Clear(LCD_COLOR_WHITE);
 }
+class Map
+{
+private:
+public:
+    void creat_map();
+    int *move_range();
+    void change_move_range();
+    int **create_space_array();
+};
 
+int **Map::create_space_array()
+{
+    int **range = new int *[SCREEN_X_LEN];
+    for (int i = 0; i < SCREEN_X_LEN; i++)
+    {
+        range[i] = new int[SCREEN_Y_LEN];
+    }
+    for (int x = 0; x < SCREEN_X_LEN; x++)
+    {
+        for (int y = 0; y < SCREEN_Y_LEN; y++)
+        {
+            range[x][y] = 0;
+        }
+    }
+    return range;
+}
 
-int main() {
+void Map::creat_map()
+{
+    int **range = create_space_array();
+    for (int x = 0; x < SCREEN_X_LEN; x++)
+    {
+        for (int y = 0; y < SCREEN_Y_LEN / 3; y++)
+        {
+            if (range[x][y] == 0)
+            {
+                BSP_LCD_DrawPixel(x, y, 51 - 51 - 255);
+            }
+        }
+    }
+}
+int main()
+{
     iniTouchscreen iniTouchscreen;
+    Map Map;
     iniTouchscreen.initial();
-    /* Set Touchscreen Demo1 description */
-    BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-    BSP_LCD_FillRect(0, 0, BSP_LCD_GetXSize(), 40);
-    BSP_LCD_SetBackColor(LCD_COLOR_GREEN);
-    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-    BSP_LCD_FillRect(0, 0, BSP_LCD_GetXSize()/2, 40);
-    BSP_LCD_SetBackColor(LCD_COLOR_YELLOW);
-    BSP_LCD_SetFont(&Font16);
     BSP_LCD_DisplayStringAt(0, 15, (uint8_t *)"Touch the screen", CENTER_MODE);
 
-    while (1) {
+    while (1)
+    {
         BSP_TS_GetState(&TS_State);
-        if(TS_State.touchDetected) {
+        if (TS_State.touchDetected)
+        {
             /* One or dual touch have been detected          */
 
             /* Get X and Y position of the first touch post calibrated */
             uint16_t x1 = TS_State.touchX[0];
             uint16_t y1 = TS_State.touchY[0];
-
             BSP_LCD_SetTextColor(LCD_COLOR_RED);
             BSP_LCD_FillCircle(x1, y1, 5);
-    
+            Map.create_space_array();
+            Map.creat_map();
             wait_ms(10);
         }
     }
+    delete []range
 }
